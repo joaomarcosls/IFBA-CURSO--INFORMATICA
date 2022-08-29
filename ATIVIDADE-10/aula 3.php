0ebbdfd2fa8eb91afb76fbc1875bed6928a10010
João Marcos Lacerda dos Santos <104016774+joaomarcosls@users.noreply.github.com>
2022-08-29 14:38:19 -0300

    Atividade 10

---
+++ ATIVIDADE-10/aula 3.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Cadastro de Cliente</title>
+       
+    </head>
+    <body>
+        <h1>Funcionou</h1>
+		<!--A seguir tera a programação php-->
+		<?php
+		
+		$joao ="joao";// essa variavel guarda o nome do usuario
+		//ok?
+		print $joao . "<br>";
+		$x =20 . "<br>";
+		echo $x . "<br>";
+		/*vai aprecer*/
+		?>
+        </form>
+    </body>
+</html>
